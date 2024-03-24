89872bd7028e9cac975d9497966657f3183e19cf
Artem Deineha <artem.deineha@techtellent.io>
2024-03-24 18:00:02 +0200

    created markup for authorization route

---
+++ src/app/authorization/utils/variables.js
@@ -0,0 +1,40 @@
+import image1 from '../../../../../assets/img/form-swiper1.png';
+import image2 from '../../../../../assets/img/form-swiper2.png';
+import image3 from '../../../../../assets/img/form-swiper3.png';
+import image4 from '../../../../../assets/img/form-swiper4.png';
+import image5 from '../../../../../assets/img/form-swiper5.png';
+import image6 from '../../../../../assets/img/form-swiper6.png';
+import image7 from '../../../../../assets/img/form-swiper7.png';
+
+const all = require('../../../../assets/img');
+
+export const imagesForSlider = [
+  {
+    imagePath: image1,
+    slideText: 'Text1'
+  },
+  {
+    imagePath: image2,
+    slideText: 'Text2'
+  },
+  {
+    imagePath: image3,
+    slideText: 'Text3'
+  },
+  {
+    imagePath: image4,
+    slideText: 'Text4'
+  },
+  {
+    imagePath: image5,
+    slideText: 'Text5'
+  },
+  {
+    imagePath: image6,
+    slideText: 'Text6'
+  },
+  {
+    imagePath: image7,
+    slideText: 'Text7'
+  }
+];
